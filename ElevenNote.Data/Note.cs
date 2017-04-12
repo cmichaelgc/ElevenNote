8735b11be2cde82246d99342325fb19dcba24d47
Charles Michael <cmichaelgc@gmail.com>
2017-04-12 16:50:07 -0400

    Built Methods for users in services.

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -13,7 +13,7 @@ namespace ElevenNote.Data
         public int NoteId { get; set; }
 
         [Required]
-        public Guid Owner { get; set; }
+        public Guid OwnerId { get; set; }
         
         [Required]
         public string Title { get; set; }
