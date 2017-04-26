5f1f1e9906b7d8f465a81570963ebb6660b35c32
Charles Michael <cmichaelgc@gmail.com>
2017-04-26 17:06:56 -0400

    Added button function on login page.

--- ElevenNote.MobileApp/ElevenNote.MobileApp/App.xaml.cs
+++ ElevenNote.MobileApp/ElevenNote.MobileApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.MobileApp.ExternalServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,16 @@ namespace ElevenNote.MobileApp
 {
 	public partial class App : Application
 	{
+        /// <summary>
+        /// Web Service access.
+        /// </summary>
+        internal static readonly NoteService NoteService = new NoteService();
+
 		public App ()
 		{
 			InitializeComponent();
 
-			MainPage = new ElevenNote.MobileApp.MainPage();
+            this.MainPage = new NavigationPage(new LoginPage());
 		}
 
 		protected override void OnStart ()
